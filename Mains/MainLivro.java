2a102da03a696e8812be03fe44db1f0263db677b
Wellington Coqueiro <163483469+WellingtonCoqueiro@users.noreply.github.com>
2025-04-09 01:48:46 -0300

    Add files via upload

---
+++ Mains/MainLivro.java
@@ -0,0 +1,26 @@
+package Mains;
+
+import Ex_2.Livro;
+
+import java.util.Scanner;
+
+public class MainLivro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o título do livro: ");
+        String titulo = scanner.nextLine();
+
+        System.out.print("Digite o autor do livro: ");
+        String autor = scanner.nextLine();
+
+        System.out.print("Digite o ano de publicação: ");
+        int ano = scanner.nextInt();
+
+        Livro livro = new Livro(titulo, autor, ano);
+
+        livro.exibirInfo();
+
+        scanner.close();
+    }
+}
