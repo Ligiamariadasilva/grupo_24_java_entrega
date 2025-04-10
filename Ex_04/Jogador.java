1d4a610024981be81aa35a12d3dcc7c0c5017157
Ligia Maria <ligiamariadasilva@gmail.com>
2025-04-09 22:01:56 -0300

    feat: adiciona exercícios da Ligia (Ex_04 a Ex_08)

---
+++ Ex_04/Jogador.java
@@ -0,0 +1,27 @@
+package Ex_04;
+
+public class Jogador {
+    private String nome;
+    private int pontuacao;
+    private int nivel;
+
+    public Jogador(String nome, int pontuacao, int nivel) {
+        this.nome = nome;
+        this.pontuacao = pontuacao;
+        this.nivel = nivel;
+    }
+
+    public void aumentarPontuacao(int pontos) {
+        this.pontuacao += pontos;
+    }
+
+    public void subirNivel() {
+        this.nivel++;
+    }
+
+    public void exibirInfo() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Pontuação: " + pontuacao);
+        System.out.println("Nível: " + nivel);
+    }
+}
